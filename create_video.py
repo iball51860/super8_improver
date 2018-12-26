18727230b1fb9fd32e5a3ea4189acc178dd3da7f
Jan Fritze <jan.fritze@student.kit.edu>
2018-12-26 15:42:40 +0100

    sorting scenes for video rendering, more verbose printout

--- create_video.py
+++ create_video.py
@@ -6,7 +6,9 @@ def createVideos(inputScenesDir: str):
     outDirectory = os.path.split(inputScenesDir)[0]
     videoOutDir = os.path.join(outDirectory, 'video')
     os.makedirs(videoOutDir)
-    for scene in [subdir for subdir in os.scandir(inputScenesDir) if os.path.isdir(subdir.path)]:
+    scenePaths = [subdir for subdir in os.scandir(inputScenesDir) if os.path.isdir(subdir.path)]
+    scenePaths.sort()
+    for scene in scenePaths:
         print('creating video for ', scene.name)
         outPath = os.path.join(videoOutDir, scene.name + '.mp4')
         createVideoFromFrames(outPath, scene.path)
@@ -14,12 +16,13 @@ def createVideos(inputScenesDir: str):
 
 def createVideoFromFrames(outPath: str, framesDirectory: str):
     framePaths = [framePath.path for framePath in os.scandir(framesDirectory) if framePath.path.endswith('.jpg')]
+    framePaths.sort()
+    print(f'writing {len(framePaths)} to {outPath}')
 
     height, width = 2408, 1770
     writer = cv2.VideoWriter(outPath, 0x21, 18, (height, width), True)
     writer.set(cv2.VIDEOWRITER_PROP_QUALITY, 1)
 
-    framePaths.sort()
     for framePath in framePaths:
         im = cv2.imread(framePath)
         writer.write(im)
